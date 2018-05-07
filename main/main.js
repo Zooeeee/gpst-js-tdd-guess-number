223a64fc41693c87e56738756d9c3f1d623155f6
[Zooeeee] <594498431@qq.com>
2018-05-07 12:27:34 +0800

    修改过的

--- main/main.js
+++ main/main.js
@@ -1,6 +1,17 @@
 // Write your cade below:
-function main() {};
+var main = function main(answer,solution) {
+    var count_a = 0 ;
+    var count_b = 0 ;
+    var n = 0 ;
+    for(var i = 0 ; i < solution.length ; i++){
+        n = answer.indexOf(solution[i]);
+        if (answer[i] === solution[i])
+        count_a ++;
+        else if (answer[i] !== solution[i] && n >= 0  )
+        count_b++;
+    }
+    
+    return count_a+"A"+count_b+"B" ;};
+module.exports = {main}
+//module.exports = main;
 
-module.exports = {
-    main
-}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,44 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+    //暂定出的随机数是1234
+    var answer = "1234";
+    var string1 = "1234";
+    var string2 = "5678";
+    var string3 = "1236";
+    var string4 = "3421";
+    var string5 = "1216";
+   
+    it("全部猜对了",function(){
+    var result = "4A0B";
+    var expected = main(answer,string1);
+    expect(expected).toEqual(result);
+    });
+   
+    it("全部猜错了",function(){
+        var result = "0A0B";
+        var expected = main(answer,string2);
+        expect(expected).toEqual(result);
+    });
+    
+    it("猜错了几个",function(){
+        var result = "3A0B";
+        var expected = main(answer,string3);
+        expect(expected).toEqual(result);
+    }); 
+    
+    it("猜对了数字，但顺序全错",function(){
+        var result = "0A4B";
+        var expected = main(answer,string4);
+        expect(expected).toEqual(result);
+    });   
+   
+    it("猜对了数字，但顺序不对",function(){
+        var result = "2A1B";
+        var expected = main(answer,string5);
+        expect(expected).toEqual(result);
+    });   
+   
+
 
 });
